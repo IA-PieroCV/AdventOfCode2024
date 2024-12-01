95cf4c0f2ad21085b638e8a2c8a0994b266a4697
IA-PieroCV <piero.casusol@intautomation.net>
2024-12-01 02:25:20 -0500

    feat: add day 1 solution

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # AdventOfCode2024
-Solution for advent of code 2024
+Solution for advent of code 2024. Using Python 3.12.7 :)
+
---
+++ day_1_historian_hysteria/main.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+with open("input.txt","r") as file:
+    lines = file.readlines()
+
+digits = map(lambda x: x.split(),lines)
+sorted_digits = list(map(sorted, zip(*digits)))
+distances = map(lambda x: abs(int(x[0])-int(x[1])),zip(*sorted_digits))
+print(sum(distances))
+
+id_counter = Counter(sorted_digits[1])
+print(sum(map(lambda x: int(x) * id_counter.get(x, 0), sorted_digits[0])))
