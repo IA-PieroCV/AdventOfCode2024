471b33c0c83db89286215a7fb999ce4444bb6d1b
IA-PieroCV <piero.casusol@intautomation.net>
2024-12-01 09:15:17 -0500

    fix: code formatted day 1

--- day_1_historian_hysteria/main.py
+++ day_1_historian_hysteria/main.py
@@ -1,11 +1,11 @@
 from collections import Counter
 
-with open("input.txt","r") as file:
+with open("input.txt", "r") as file:
     lines = file.readlines()
 
-digits = map(lambda x: x.split(),lines)
+digits = map(lambda x: x.split(), lines)
 sorted_digits = list(map(sorted, zip(*digits)))
-distances = map(lambda x: abs(int(x[0])-int(x[1])),zip(*sorted_digits))
+distances = map(lambda x: abs(int(x[0]) - int(x[1])), zip(*sorted_digits))
 print(sum(distances))
 
 id_counter = Counter(sorted_digits[1])
